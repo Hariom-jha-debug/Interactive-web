0fa262a45094f09e249b7a7695461bbd33f3a0b0
Hariom-jha-debug <hjha6656@gmail.com>
2026-01-03 21:58:04 +0530

    Create App component for interactive counter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from './components/Counter.jsx';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Interactive Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
